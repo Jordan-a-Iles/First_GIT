eeef869ff4862882aaf502cd9e7a1c25ae765826
Jordan Iles <jc559751@ad.jcu.edu.au>
2020-05-15 13:13:01 +1000

    First commit. Added header_template script

---
+++ scripts/header_template.R
@@ -0,0 +1,16 @@
+# Script name: Generic header script
+# Purpose: Generic header to be copied and used on later scripts
+# Author: Jordan Iles
+# Date created: 2019-05-15
+# Contact: jordan.iles@jcu.edu.au
+
+#### Clear workspace ----
+rm(list = ls(all.names = TRUE))
+
+#### load required packages ----
+require(tidyverse)
+
+#### Set working directory ----
+setwd('~/../Dropbox/JCU/R/FirstGIT')
+
+#### Start coding! ----
